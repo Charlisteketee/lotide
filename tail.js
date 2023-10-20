eba581057b1490358d77f66d02b3878f361d5222
Charlisteketee <charlisteketee@gmail.com>
2023-10-20 11:42:26 -0700

    Add tail function

---
+++ tail.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: \u{1F60A}`);
+  } else {
+    console.log(`Assertion Failed: \u{1F641}`);
+  }
+};
+assertEqual("Lighthouse", "Lighthouse");
+assertEqual(1, "one");
+
+function tail(arr) {
+  if (arr.length > 1) {
+    return arr.slice(1);
+  } else {
+    return [];
+  }
+}
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+
+//test
+const words = ["yo yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length,);
