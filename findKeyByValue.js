3bca308df6665fad4e193d49c00b9a7473875b05
Charlisteketee <charlisteketee@gmail.com>
2023-10-25 15:09:03 -0700

    add find key funtion by value

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: \u{1F60A}`);
+  } else {
+    console.log(`Assertion Failed: \u{1F641}`);
+  }
+};
+
+const findKeyByValue = function(genre, tvShow) { // function to find a key by the value
+  const results = (genre);
+
+  for (const key in genre) {
+    if (genre[key] === tvShow) {
+      return key;
+    }
+  }
+return results;
+};  
+
+//tests
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  mystery: "Lupin"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
