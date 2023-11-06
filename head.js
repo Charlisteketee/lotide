59cd007af1f384659fc8be943c7c4f95f2fb648b
Charlisteketee <charlisteketee@gmail.com>
2023-11-06 10:51:54 -0800

    took out testers and require assertEqual

--- head.js
+++ head.js
@@ -1,18 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: \u{1F60A}`);
-  } else {
-    console.log(`Assertion Failed: \u{1F641}`);
-  }
-};
-assertEqual("Lighthouse", "Lighthouse");
-assertEqual(1, "one");
+
+const assertEqual = require('./assertEqual');
 
 //return the 'head' of the array as an element
 function head(arr) {
   return arr[0];
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
+module.exports = head;
