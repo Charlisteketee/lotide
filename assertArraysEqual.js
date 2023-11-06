459f21c2114941ab11419f2563c41e4c83765b8c
Charlisteketee <charlisteketee@gmail.com>
2023-11-06 13:00:50 -0800

    removed tester code to new test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,26 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: \u{1F60A}`);
-  } else {
-    console.log(`Assertion Failed: \u{1F641}`);
-  }
-};
-assertEqual("Lighthouse", "Lighthouse");
-assertEqual(1, "one");
+const eqArrays = require('./eqArrays');
 
-// iterate through the arrays and compare each element
-const eqArrays = function(arr1, arr2) { 
-  for (let i = 0; i <arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)
 
 const assertArrayEqual = function(arr1, arr2) { 
   if (eqArrays(arr1, arr2)) {
@@ -29,3 +8,5 @@ const assertArrayEqual = function(arr1, arr2) {
     console.log("❌ Arrays are not equal.");
   }
 }
+
+module.exports = assertArrayEqual;
