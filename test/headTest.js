0f9f888858849ed0c47d19b636e42d39e4212ee9
Charlisteketee <charlisteketee@gmail.com>
2023-11-06 13:38:55 -0800

    improve test code with Mocha and Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,21 @@
 const head = require('../head');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
+//Tests
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
+  it("should return '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("should return 5 for[5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it("should return 'howdy' for ['howdy', 'lighthouse', 'labs']", () => {
+    assert.strictEqual(head(['howdy', 'lighthouse', 'labs']), 'howdy');
+  })
+});
