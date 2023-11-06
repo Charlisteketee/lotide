35babfe171870d069fabae7152c7f37344cff0b0
Charlisteketee <charlisteketee@gmail.com>
2023-11-06 13:11:06 -0800

    removed test code

--- middle.js
+++ middle.js
@@ -1,22 +1,3 @@
-// iterate through the arrays and compare each element
-const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-//implements the eqArray function to check if arrays are equal
-const assertArrayEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log("✅ Arrays are equal!");
-  } else {
-    console.log("❌ Arrays are not equal.");
-  }
-};
-
 //Add elements of the array to find median
 const middle = function(arr) {
   const middleIndex = Math.floor(arr.length / 2);
@@ -30,8 +11,7 @@ const middle = function(arr) {
   }
 };
 
-//tester
-const result = middle([1, 2, 3, 4, 5, 6])
-assertArrayEqual(result, [3, 4]);
+module.exports = middle;
+
 
 
